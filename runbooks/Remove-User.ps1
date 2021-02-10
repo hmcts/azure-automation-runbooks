d1e1a1cb711e3089c25edb044fd01b138e628f05
Matt Slater <matt.slater@hmcts.net>
2021-02-10 10:10:02 +0000

    DTSPO-440 - create runbook

---
+++ runbooks/Remove-User.ps1
@@ -0,0 +1,55 @@
+param
+(
+    [Parameter (Mandatory = $false)]
+    [object] $webhookData
+)
+
+$subscriptionName = "DCD-CNP-DEV"
+
+function Get-TriggerBuild {
+    param([string]$namespace)
+
+    # Azure DevOps token
+    $token = ( Get-AzureKeyVaultSecret -vaultName "infra-vault-nonprod" -name "azure-devops-token" ).SecretValueText
+    
+    # Authentication - username:token. The username can be anything, the token is a Personal Access Token in an Azure DevOps account
+    $plainTextCreds = "hmcts@hmcts.net:$token"
+
+    # Azure DevOps Endpoint.  This includes the org and project name of the build we're triggering
+    $build_definition = 449
+    $azureDevOpsEndpoint = "https://dev.azure.com/hmcts/PlatformOperations/_apis/pipelines/$build_definition/runs?api-version=6.0-preview.1"
+
+    # Get the UPN from the webhook payload
+    $requestBody = (ConvertFrom-Json -InputObject $webhookData.RequestBody)
+    $upn = $requestBody.data.alertContext.SearchResults.tables.rows[0]
+
+    # HTTP request body
+    $Body = @"
+{
+	"stagesToSkip": [],
+	"templateParameters": {
+		"upn": "$upn"
+	}
+}
+"@
+
+    #Prepare credentials
+    $AuthBytes  = [System.Text.Encoding]::Ascii.GetBytes($plainTextCreds)
+    $base64AuthInfo = [Convert]::ToBase64String($AuthBytes)
+
+    $Result = Invoke-RestMethod -Uri $azureDevOpsEndpoint -Method Post -ContentType "application/json" -Body $body -Headers @{"Authorization"="Basic $base64AuthInfo"} 
+    Write-Output $Result
+}
+
+
+if ($webhookData) {
+    #Authenticate as "Run-As Account" using certificate
+    $connection = Get-AutomationConnection -Name AzureRunAsConnection
+    Add-AzureRMAccount -ServicePrincipal -Tenant $connection.TenantID -ApplicationId $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
+    Select-AzureRMSubscription -SubscriptionName $subscriptionName
+    
+    # Trigger the build
+    Trigger-Build
+
+}
+
